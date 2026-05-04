55bce47983d231508199fecc7aacbb9e1ed4ea88
JasonBuenvasita-dev <buenavistajasonjonh@gmail.com>
2026-05-04 21:30:59 +0800

    Refactor frmAddItem layout and add header panel
    Updated the form layout and added a header panel for better UI.

--- Office_inventory/Form2.Designer.vb
+++ Office_inventory/Form2.Designer.vb
@@ -1,7 +1,8 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmAddItem
     Inherits System.Windows.Forms.Form
 
+    'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
@@ -17,6 +18,8 @@ Partial Class frmAddItem
 
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.pnlHeader = New System.Windows.Forms.Panel()
+        Me.lblHeaderTitle = New System.Windows.Forms.Label()
         Me.txtItemName = New System.Windows.Forms.TextBox()
         Me.txtPrice = New System.Windows.Forms.TextBox()
         Me.txtMinStock = New System.Windows.Forms.TextBox()
@@ -31,125 +34,159 @@ Partial Class frmAddItem
         Me.lblPrice = New System.Windows.Forms.Label()
         Me.lblMinStock = New System.Windows.Forms.Label()
         Me.lblBarcode = New System.Windows.Forms.Label()
+        Me.pnlHeader.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'pnlHeader
+        '
+        Me.pnlHeader.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.pnlHeader.Controls.Add(Me.lblHeaderTitle)
+        Me.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlHeader.Location = New System.Drawing.Point(0, 0)
+        Me.pnlHeader.Name = "pnlHeader"
+        Me.pnlHeader.Size = New System.Drawing.Size(400, 65)
+        Me.pnlHeader.TabIndex = 0
+        '
+        'lblHeaderTitle
+        '
+        Me.lblHeaderTitle.AutoSize = True
+        Me.lblHeaderTitle.Font = New System.Drawing.Font("Segoe UI Semilight", 16.0!)
+        Me.lblHeaderTitle.ForeColor = System.Drawing.Color.White
+        Me.lblHeaderTitle.Location = New System.Drawing.Point(18, 15)
+        Me.lblHeaderTitle.Name = "lblHeaderTitle"
+        Me.lblHeaderTitle.Size = New System.Drawing.Size(185, 37)
+        Me.lblHeaderTitle.TabIndex = 0
+        Me.lblHeaderTitle.Text = "Add New Item"
         '
         'txtItemName
         '
-        Me.txtItemName.Location = New System.Drawing.Point(130, 17)
+        Me.txtItemName.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.txtItemName.Location = New System.Drawing.Point(145, 85)
         Me.txtItemName.Name = "txtItemName"
-        Me.txtItemName.Size = New System.Drawing.Size(200, 22)
+        Me.txtItemName.Size = New System.Drawing.Size(230, 30)
         Me.txtItemName.TabIndex = 1
-        '
-        'txtPrice
-        '
-        Me.txtPrice.Location = New System.Drawing.Point(130, 137)
-        Me.txtPrice.Name = "txtPrice"
-        Me.txtPrice.Size = New System.Drawing.Size(200, 22)
-        Me.txtPrice.TabIndex = 4
-        '
-        'txtMinStock
-        '
-        Me.txtMinStock.Location = New System.Drawing.Point(130, 177)
-        Me.txtMinStock.Name = "txtMinStock"
-        Me.txtMinStock.Size = New System.Drawing.Size(200, 22)
-        Me.txtMinStock.TabIndex = 5
-        '
-        'txtBarcode
-        '
-        Me.txtBarcode.Location = New System.Drawing.Point(130, 217)
-        Me.txtBarcode.Name = "txtBarcode"
-        Me.txtBarcode.Size = New System.Drawing.Size(200, 22)
-        Me.txtBarcode.TabIndex = 6
         '
         'cmbCategory
         '
-        Me.cmbCategory.FormattingEnabled = True
+        Me.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbCategory.Font = New System.Drawing.Font("Segoe UI", 10.0!)
         Me.cmbCategory.Items.AddRange(New Object() {"Office Supplies", "Electronics", "Furniture", "Other"})
-        Me.cmbCategory.Location = New System.Drawing.Point(130, 57)
+        Me.cmbCategory.Location = New System.Drawing.Point(145, 125)
         Me.cmbCategory.Name = "cmbCategory"
-        Me.cmbCategory.Size = New System.Drawing.Size(200, 24)
+        Me.cmbCategory.Size = New System.Drawing.Size(230, 31)
         Me.cmbCategory.TabIndex = 2
         '
         'cmbUnit
         '
-        Me.cmbUnit.FormattingEnabled = True
+        Me.cmbUnit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbUnit.Font = New System.Drawing.Font("Segoe UI", 10.0!)
         Me.cmbUnit.Items.AddRange(New Object() {"Piece", "Box", "Pack", "Ream", "Set"})
-        Me.cmbUnit.Location = New System.Drawing.Point(130, 97)
+        Me.cmbUnit.Location = New System.Drawing.Point(145, 165)
         Me.cmbUnit.Name = "cmbUnit"
-        Me.cmbUnit.Size = New System.Drawing.Size(200, 24)
+        Me.cmbUnit.Size = New System.Drawing.Size(230, 31)
         Me.cmbUnit.TabIndex = 3
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.txtPrice.Location = New System.Drawing.Point(145, 205)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(230, 30)
+        Me.txtPrice.TabIndex = 4
+        '
+        'txtMinStock
+        '
+        Me.txtMinStock.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.txtMinStock.Location = New System.Drawing.Point(145, 245)
+        Me.txtMinStock.Name = "txtMinStock"
+        Me.txtMinStock.Size = New System.Drawing.Size(230, 30)
+        Me.txtMinStock.TabIndex = 5
+        '
+        'txtBarcode
+        '
+        Me.txtBarcode.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.txtBarcode.Location = New System.Drawing.Point(145, 285)
+        Me.txtBarcode.Name = "txtBarcode"
+        Me.txtBarcode.Size = New System.Drawing.Size(230, 30)
+        Me.txtBarcode.TabIndex = 6
         '
         'btnSave
         '
-        Me.btnSave.Location = New System.Drawing.Point(130, 270)
+        Me.btnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(122, Byte), Integer), CType(CType(204, Byte), Integer))
+        Me.btnSave.FlatAppearance.BorderSize = 0
+        Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSave.Font = New System.Drawing.Font("Segoe UI Semibold", 9.5!)
+        Me.btnSave.ForeColor = System.Drawing.Color.White
+        Me.btnSave.Location = New System.Drawing.Point(145, 345)
         Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(90, 30)
+        Me.btnSave.Size = New System.Drawing.Size(110, 40)
         Me.btnSave.TabIndex = 7
-        Me.btnSave.Text = "Save"
-        Me.btnSave.UseVisualStyleBackColor = True
+        Me.btnSave.Text = "Save Item"
+        Me.btnSave.UseVisualStyleBackColor = False
         '
         'btnCancel
         '
-        Me.btnCancel.Location = New System.Drawing.Point(240, 270)
+        Me.btnCancel.BackColor = System.Drawing.Color.White
+        Me.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCancel.Font = New System.Drawing.Font("Segoe UI", 9.5!)
+        Me.btnCancel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnCancel.Location = New System.Drawing.Point(265, 345)
         Me.btnCancel.Name = "btnCancel"
-        Me.btnCancel.Size = New System.Drawing.Size(90, 30)
+        Me.btnCancel.Size = New System.Drawing.Size(110, 40)
         Me.btnCancel.TabIndex = 8
         Me.btnCancel.Text = "Cancel"
-        Me.btnCancel.UseVisualStyleBackColor = True
+        Me.btnCancel.UseVisualStyleBackColor = False
         '
         'lblItemName
         '
-        Me.lblItemName.Location = New System.Drawing.Point(20, 20)
-        Me.lblItemName.Name = "lblItemName"
-        Me.lblItemName.Size = New System.Drawing.Size(100, 23)
-        Me.lblItemName.TabIndex = 0
-        Me.lblItemName.Text = "Item Name:"
+        Me.lblItemName.AutoSize = True
+        Me.lblItemName.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.lblItemName.Location = New System.Drawing.Point(25, 90)
+        Me.lblItemName.Text = "Item Name"
         '
         'lblCategory
         '
-        Me.lblCategory.Location = New System.Drawing.Point(20, 60)
-        Me.lblCategory.Name = "lblCategory"
-        Me.lblCategory.Size = New System.Drawing.Size(100, 23)
-        Me.lblCategory.TabIndex = 2
-        Me.lblCategory.Text = "Category:"
+        Me.lblCategory.AutoSize = True
+        Me.lblCategory.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.lblCategory.Location = New System.Drawing.Point(25, 130)
+        Me.lblCategory.Text = "Category"
         '
         'lblUnit
         '
-        Me.lblUnit.Location = New System.Drawing.Point(20, 100)
-        Me.lblUnit.Name = "lblUnit"
-        Me.lblUnit.Size = New System.Drawing.Size(100, 23)
-        Me.lblUnit.TabIndex = 3
-        Me.lblUnit.Text = "Unit:"
+        Me.lblUnit.AutoSize = True
+        Me.lblUnit.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.lblUnit.Location = New System.Drawing.Point(25, 170)
+        Me.lblUnit.Text = "Unit"
         '
         'lblPrice
         '
-        Me.lblPrice.Location = New System.Drawing.Point(20, 140)
-        Me.lblPrice.Name = "lblPrice"
-        Me.lblPrice.Size = New System.Drawing.Size(100, 23)
-        Me.lblPrice.TabIndex = 4
-        Me.lblPrice.Text = "Cost Price:"
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.lblPrice.Location = New System.Drawing.Point(25, 210)
+        Me.lblPrice.Text = "Cost Price"
         '
         'lblMinStock
         '
-        Me.lblMinStock.Location = New System.Drawing.Point(20, 180)
-        Me.lblMinStock.Name = "lblMinStock"
-        Me.lblMinStock.Size = New System.Drawing.Size(100, 23)
-        Me.lblMinStock.TabIndex = 5
-        Me.lblMinStock.Text = "Min Stock:"
+        Me.lblMinStock.AutoSize = True
+        Me.lblMinStock.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.lblMinStock.Location = New System.Drawing.Point(25, 250)
+        Me.lblMinStock.Text = "Min Stock"
         '
         'lblBarcode
         '
-        Me.lblBarcode.Location = New System.Drawing.Point(20, 220)
-        Me.lblBarcode.Name = "lblBarcode"
-        Me.lblBarcode.Size = New System.Drawing.Size(100, 23)
-        Me.lblBarcode.TabIndex = 6
-        Me.lblBarcode.Text = "Barcode:"
+        Me.lblBarcode.AutoSize = True
+        Me.lblBarcode.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.lblBarcode.Location = New System.Drawing.Point(25, 290)
+        Me.lblBarcode.Text = "Barcode"
         '
         'frmAddItem
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(380, 330)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(400, 410)
+        Me.Controls.Add(Me.pnlHeader)
         Me.Controls.Add(Me.lblItemName)
         Me.Controls.Add(Me.txtItemName)
         Me.Controls.Add(Me.lblCategory)
@@ -164,9 +201,15 @@ Partial Class frmAddItem
         Me.Controls.Add(Me.txtBarcode)
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.btnCancel)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "frmAddItem"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
-        Me.Text = "Add New Item"
+        Me.Text = "Product Management"
+        Me.pnlHeader.ResumeLayout(False)
+        Me.pnlHeader.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -186,5 +229,6 @@ Partial Class frmAddItem
     Friend WithEvents lblPrice As Label
     Friend WithEvents lblMinStock As Label
     Friend WithEvents lblBarcode As Label
-
+    Friend WithEvents pnlHeader As Panel
+    Friend WithEvents lblHeaderTitle As Label
 End Class
